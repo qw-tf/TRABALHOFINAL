61c2fbc9964bf9911026f75ff738cf0ddcf4d3bd
Brandao-GBL <qwp16274@gmail.com>
2025-02-12 19:45:16 -0300

    COMMIT-FINAL

--- app.java
+++
@@ -1,9 +0,0 @@
-public class app {
-    public static void main(String[] args) {
-        ControladorDeEstoque controlador = new ControladorDeEstoque();
-        ManipularArquivo manipulador = new ManipularArquivo("produtos.csv", controlador);
-        manipulador.carregarProdutos();
-        controlador.adicionarProduto();
-        manipulador.salvarArquivo();
-    }
-}
